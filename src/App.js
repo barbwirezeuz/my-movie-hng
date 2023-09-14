d645ca89702a1d5cb29b770866ab88b6a4bc8e39
barbwirezeuz <soceeenin@yahoo.com>
2023-09-14 20:23:41 +0100

    chnages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,117 @@
+// import React, { useState, useEffect } from 'react';
+// import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+// import './styles.css';
+// import MovieDetailsPage from './components/MovieDetailsPage';
+// import HomePage from './components/HomePage';
+
+// function App() {
+//   const [searchTerm, setSearchTerm] = useState('');
+//   const [movies, setMovies] = useState([]);
+//   const [error, setError] = useState(null);
+
+  
+//   useEffect(() => {
+//     const apiKey = '9384d374068a0ca81428ae26faf2df18';
+//     const topRatedUrl = `https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}&page=1`;
+
+//     async function fetchTopRatedMovies() {
+//       try {
+//         const response = await fetch(topRatedUrl);
+//         if (!response.ok) {
+//           throw new Error('Network response was not ok');
+//         }
+//         const data = await response.json();
+//         setMovies(data.results.slice(0, 10));
+//         setError(null);
+//       } catch (error) {
+//         setError('Error fetching data. Please try again later.');
+//         console.error('Error fetching data:', error);
+//       }
+//     }
+
+//     fetchTopRatedMovies();
+//   }, []);
+
+  
+//   useEffect(() => {
+//     if (searchTerm) {
+//       const apiKey = '9384d374068a0ca81428ae26faf2df18';
+//       const searchUrl = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${searchTerm}`;
+
+//       async function fetchMovies() {
+//         try {
+//           const response = await fetch(searchUrl);
+//           if (!response.ok) {
+//             throw new Error('Network response was not ok');
+//           }
+//           const data = await response.json();
+//           setMovies(data.results);
+//           setError(null);
+//         } catch (error) {
+//           setError('Error fetching data. Please try again later.');
+//           console.error('Error fetching data:', error);
+//         }
+//       }
+
+//       fetchMovies();
+//     }
+//   }, [searchTerm]);
+
+//   return (
+//     <Router>
+//       <div className="App">
+//         {/* Header */}
+//         <header className="header" >
+//           {/* <div className="title">MovieBox</div> */}
+//           {/* <div className="search-bar">
+//             <input
+//               type="text"
+//               placeholder="What do you want to watch ?"
+//               value={searchTerm}
+//               onChange={(e) => setSearchTerm(e.target.value)}
+//             />
+//           </div> */}
+//           {/* <div className="sign-in-link">
+//             <Link to="/">Sign In</Link>
+//           </div>
+//           <div className="menu-button">
+//             <button>&#9776;</button>
+//           </div> */}
+//         </header>
+//         <div className="main-content">
+//           {error && <p>{error}</p>}
+//           <Routes>
+//             <Route path="/" element={<HomePage movies={movies} />} />
+//             <Route path="/movies/:id" element={<MovieDetailsPage />} />
+//           </Routes>
+//         </div>
+//       </div>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
+
+
+
+
+
+
+
+
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link, useLocation } from 'react-router-dom';
 import './styles.css';
 import MovieDetailsPage from './components/MovieDetailsPage';
 import HomePage from './components/HomePage';
+import Header from './components/Header'; 
 
 function App() {
   const [searchTerm, setSearchTerm] = useState('');
   const [movies, setMovies] = useState([]);
   const [error, setError] = useState(null);
 
-  
   useEffect(() => {
     const apiKey = '9384d374068a0ca81428ae26faf2df18';
     const topRatedUrl = `https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}&page=1`;
@@ -32,7 +134,6 @@ function App() {
     fetchTopRatedMovies();
   }, []);
 
-  
   useEffect(() => {
     if (searchTerm) {
       const apiKey = '9384d374068a0ca81428ae26faf2df18';
@@ -60,34 +161,21 @@ function App() {
   return (
     <Router>
       <div className="App">
-        {/* Header */}
-        <header className="header" >
-          {/* <div className="title">MovieBox</div> */}
-          {/* <div className="search-bar">
-            <input
-              type="text"
-              placeholder="What do you want to watch ?"
-              value={searchTerm}
-              onChange={(e) => setSearchTerm(e.target.value)}
-            />
-          </div> */}
-          {/* <div className="sign-in-link">
-            <Link to="/">Sign In</Link>
-          </div>
-          <div className="menu-button">
-            <button>&#9776;</button>
-          </div> */}
-        </header>
+        <Routes>
+          <Route path="/" element={<Header />} /> 
+          <Route path="/" element={<HomePage movies={movies} />} />
+          {/* <Route path="/movies/:id" element={<MovieDetailsPage />} /> */}
+        </Routes>
+
         <div className="main-content">
-          {error && <p>{error}</p>}
-          <Routes>
+           {error && <p>{error}</p>}
+           <Routes>
             <Route path="/" element={<HomePage movies={movies} />} />
             <Route path="/movies/:id" element={<MovieDetailsPage />} />
           </Routes>
         </div>
-      </div>
-    </Router>
-  );
-}
-
+       </div>
+     </Router>
+   );
+ }
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+// Header.js
+import React from 'react';
+
+function Header() {
+  return (
+    <header className="header">
+      {/* Header content with background photo */}
+    </header>
+  );
+}
+
+export default Header;
